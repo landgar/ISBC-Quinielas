a50a00f8475d96a806ad472ba4bf1afafbd6a13e
Raul Marcos <p50kobe@gmail.com>
2012-11-04 00:18:06 +0100

    Data for seasons, with a method in Testeo.java

--- src/isbc/quinielas/Testeo.java
+++ src/isbc/quinielas/Testeo.java
@@ -1,9 +1,11 @@
 package isbc.quinielas;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.StringTokenizer;
 
@@ -40,10 +42,38 @@ public class Testeo {
 		}
 	}
 	
+	protected static void fileSpliter(File file){
+		try {
+			String line = "";
+			FileReader fr = new FileReader(file);
+			BufferedReader br = new BufferedReader(fr);
+			BufferedWriter bw;
+
+			for (int i = 1995; i<2013; i++){
+
+				String pathname = "./data/season_" + i + ".txt";
+				FileWriter fw = new FileWriter(pathname);
+				bw = new BufferedWriter(fw);
+
+				int currentSeason = i;
+				while (currentSeason == i){
+					line = br.readLine();
+					currentSeason = Integer.valueOf(line.substring(0, 4));
+					System.out.println("season: " + currentSeason);
+					if (currentSeason == i){
+						System.out.println("line: " + line);
+						bw.write(line);
+						bw.write("\n");
+					}
+				}
+				bw.close();
+			}
+		} catch (Exception e){}
+	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		File data = new File("./data/baseData.txt");
-		fileTesting(data);
+		//fileSpliter(data);
 	}
 }
